374ee53e07e64d30c488c5107cf62ae492d0c6cb
Your Name <you@example.com>
2023-05-05 15:28:52 +0700

    product

--- src/main/java/com/example/demo/entities/Product.java
+++ src/main/java/com/example/demo/entities/Product.java
@@ -3,14 +3,18 @@ package com.example.demo.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String price;
+    private int quantity;
     private int status;
 }
